20f7070bda9f36e6d6d9f34cbd4a7aa061989803
MhmedTarekRadwan <mhmedtarekradwan@gmail.com>
2026-09-22 13:45:22 +0300

    solve some problem

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Problem1
+            //Console.WriteLine($"Monday = {(int)Weekdays.Monday}");
+            //Console.WriteLine($"Tuesday = {(int)Weekdays.Tuesday}");
+            //Console.WriteLine($"Wednesday = {(int)Weekdays.Wednesday}");
+            //Console.WriteLine($"Thursday = {(int)Weekdays.Thursday}");
+            //Console.WriteLine($"Friday = {(int)Weekdays.Friday}");
+
+            //Console.WriteLine("*****************************");
+
+            //foreach (Weekdays day in Enum.GetValues(typeof(Weekdays)))
+            //{
+            //    Console.WriteLine($"{day} = {(int)day}");
+            //} 
+            #endregion
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task09/Weekdays.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task09
+{
+    public enum Weekdays
+    {
+        Monday = 1,
+        Tuesday ,
+        Wednesday ,
+        Thursday ,
+        Friday ,
+        
+
+    }
+}
